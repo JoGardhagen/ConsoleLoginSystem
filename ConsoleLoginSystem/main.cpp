acf10dab17b9261715f00b5ed5ff45472e0d6751
Joakim <superapa.spanx@gmail.com>
2023-12-23 22:42:52 +0100

    init

---
+++ ConsoleLoginSystem/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  ConsoleLoginSystem
+//
+//  Created by Joakim Gårdhagen on 2023-12-23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    
+    return 0;
+}
