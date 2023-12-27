2020e37634c09b8d78d9ebdd910130dca28bcba6
Joakim <superapa.spanx@gmail.com>
2023-12-27 14:46:49 +0100

    starting

--- ConsoleLoginSystem/main.cpp
+++ ConsoleLoginSystem/main.cpp
@@ -14,19 +14,40 @@
 int main(int argc, const char * argv[]) {
     
     UserManager usermanager;
+    int choice;
     
-    std::string regUserName,regPassword;
-    std::cout<<"Enter Username for registration: ";
-    std::cin>>regUserName;
-    std::cout<<"Enter Password for registration: ";
-    std::cin>>regPassword;
-    
-    usermanager.registerUser(regUserName, regPassword);
-    
-    
-    
-
-    
-    
+    do {
+        std::cout<<"Menu:\n";
+        std::cout<<"1: Register\n";
+        std::cout<<"2: Login\n";
+        std::cout<<"3: Exit\n";
+        std::cout<<"Enter your choice:";
+        std::cin>>choice;
+        
+        switch(choice){
+            case 1:{
+                std::string regUsername,regPassword;
+                std::cout<< "Enter username for registration: ";
+                std::cin>>regUsername;
+                std::cout<<"Enter password for registration: ";
+                std::cin>>regPassword;
+                usermanager.registerUser(regUsername, regPassword);
+                break;
+            }
+            case 2:{
+                std::string loginUsername,LoginPassword;
+                std::cout<<"Enter username for login: ";
+                std::cin>>loginUsername;
+                std::cout<<"Enter password for login: ";
+                std::cin>>LoginPassword;
+                usermanager.loginUser(loginUsername, LoginPassword);
+                break;
+            }
+            case 3:{
+                std::cout<<"Exiting Porgram.\n";
+            }
+        }
+        
+    } while (choice !=3);
     return 0;
 }
