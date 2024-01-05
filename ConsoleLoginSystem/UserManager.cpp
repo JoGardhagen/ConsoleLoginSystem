7253f7f8027f468af9f4d96571854c5249806615
Joakim <superapa.spanx@gmail.com>
2024-01-05 17:25:01 +0100

    1

--- ConsoleLoginSystem/UserManager.cpp
+++ ConsoleLoginSystem/UserManager.cpp
@@ -90,7 +90,7 @@ std::shared_ptr<User> UserManager::getLoggedInUser() {
     return loggedInUser;
 }
 
-void UserManager::changePassword(const std::string& newPassword){
+/*void UserManager::changePassword(const std::string& newPassword){
     if(loggedInUser){
         //for(auto& user : users){
             std::cout << "Changing password for user: " << loggedInUser->getUsername() << std::endl;
@@ -103,11 +103,73 @@ void UserManager::changePassword(const std::string& newPassword){
     }else{
         std::cout<<"Didnt change. . .\n";
     }
+}*/
+/*void UserManager::changePassword(const std::string& newPassword){
+    if(loggedInUser){
+        loggedInUser->setPassword(newPassword);
+        
+        std::ifstream inputFile("userDB.txt");
+        std::ofstream outoutFile("userDB_temp.txt");
+        
+        if(inputFile.is_open() && outoutFile.is_open()){
+            std::string username,password,dateCreated;
             
-    //}
+            while(inputFile>>username>>password>>dateCreated){
+                if (username==loggedInUser->getUsername()) {
+                    outoutFile<<loggedInUser->getUsername()<<" "<<newPassword<<" "<< loggedInUser->getDateCreated()<<"\n";
+                }else{
+                    outoutFile << username <<" "<< password <<" "<<dateCreated<<'\n';
+                }
+            }
+            inputFile.close();
+            outoutFile.close();
+            
+            std::remove("userDB.txt");
+            std::rename("userDB_temp.txt","userDB.txt");
+            std::cout<<"Password changed.\n";
+        }else{
+            std::cerr<<"Unable to open for read or write from or to files.\n";
+        }
+    }else{
+        std::cout<<"Not logged in, unable to change password.\n";
+    }
+}*/
+void UserManager::changePassword(const std::string& newPassword){
+    if(loggedInUser != nullptr){
+        std::ifstream inFile("userDB.txt");
+        if(!inFile.is_open()){
+            std::cerr<<"Unable to open userDB.txt for reading.\n";
+            return;
+        }
+        std::vector<std::string>lines;
+        std::string line;
+        while(std::getline(inFile, line)){
+            lines.push_back(line);
+        }
+        inFile.close();
         
-        
-    
+        std::ofstream outFile("userDB.txt");
+        if(!outFile.is_open()){
+            std::cerr<<"Unable to open userDB.txt for writing.\n";
+            return;
+        }
+        for(const std::string& storedUser : lines){
+            std::istringstream iss(storedUser);
+            std::string storedUsername, storedPassword, storedDateCreated;
+            if(iss >> storedUsername >> storedPassword >> storedDateCreated){
+                if(loggedInUser->getUsername()==storedUsername){
+                    outFile << storedUsername << " " << newPassword << " " << storedDateCreated << '\n';
+                }else{
+                    outFile << storedUser<<"\n";
+                }
+                
+            }
+        }
+        outFile.close();
+        std::cout<<"Password changed.\n";
+    }else{
+        std::cout<<"Not logged in.\n";
+    }
 }
 
 
